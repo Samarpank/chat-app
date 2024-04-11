3073cb76d8f6a99563b1b8c82044a3902b40bafb
Shubham Gupta <shubham.raj4611@gmail.com>
2024-04-11 09:38:17 +0530

    2nd-part completed-ui design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,13 @@
 import "./App.css";
+import Login from "./pages/login/Login";
+import SignUp from './pages/signup/SignUp';
+import Home from "./pages/home/Home";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-       <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
-    </>
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <Home/> 
+    </div>
   );
 }
 
-export default App
+export default App;
