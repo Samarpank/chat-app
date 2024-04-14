d6433b837aede56b73fa0f2f11ce56b0dccc218e
Shubham Gupta <shubham.raj4611@gmail.com>
2024-04-14 11:09:44 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
@@ -7,8 +8,8 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import {app, server} from './socket/socket.js';
 
-const app=express();
 const PORT=process.env.PORT||5000;
 
 dotenv.config();
@@ -25,7 +26,7 @@ app.use("/api/users",userRoutes);
 // });
 
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     connectToMongoDB();
     console.log(`Server Running on port ${PORT}`)
 });
