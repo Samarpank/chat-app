f4b7809d61c8ad060a33224237b615e6c8311e2d
Shubham Gupta <shubham.raj4611@gmail.com>
2024-04-14 11:09:37 +0530

    4th part completed

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId } from "../socket/socket.js";
 
 
 export const sendMessage = async (req, res) => {
@@ -28,9 +29,15 @@ export const sendMessage = async (req, res) => {
 			conversation.messages.push(newMessage._id);
 		}
 
+
+
 		
 		await Promise.all([conversation.save(), newMessage.save()]);
 
+		const receiverSocketId=getReceiverSocketId(receiverId);
+        if(receiverSocketId){
+			io.to(receiverSocketId).emit("newMessage",newMessage)
+		}
 		
 
 		res.status(201).json(newMessage);
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,20 +1,33 @@
-import Message from "./Message"
+import { useEffect ,useRef} from "react";
+import UseGetMessages from "../../hooks/UseGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
+import Message from "./Message";
+import useListenMessages from "../../hooks/useListenMessages";
 
 const Messages = () => {
+     const {messages,loading}=UseGetMessages();
+     useListenMessages();
+     const lastMessageRef=useRef();
+     useEffect(()=>{
+      setTimeout(()=>{
+         lastMessageRef.current?.scrollIntoView({behavior:"smooth"});
+      },100);
+     },[messages]);
   return (
     <div className='px-4 flex-1 overflow-auto'>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-    </div>
-  )
-}
+        {!loading && messages.length>0 && messages.map((message)=>(
+          <div key={message._id} ref={lastMessageRef}>
+          <Message  message={message}/>
+          </div>
+        ))}
 
-export default Messages
+        {loading && [...Array(3)].map((_, idx)=> <MessageSkeleton key={idx}/>)}
+
+        {!loading && messages.length === 0 && (
+          <p className='text-center'>Send a message to start the conversation</p>
+        )}
+    </div>
+  );
+};
+
+export default Messages;
